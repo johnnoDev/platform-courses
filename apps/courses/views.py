b2d0430892bf2dddf32c7af575f63c1be86a84fa
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-01 18:49:19 -0500

    avance del proyecto: urls, views, loads, ect

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def course_list(request):
+    return render(request, 'courses/courses.html')
+
+def course_detail(request):
+    return render(request, 'courses/course_detail.html')
+
+def course_lessons(request):
+    pass

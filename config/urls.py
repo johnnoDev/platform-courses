963837cadc871259404d381263c939995b0dc6ae
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-03 08:22:45 -0500

    dasboard incluido

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('courses/', include('apps.courses.urls'))
+    path('courses/', include('apps.courses.urls')),
+    path('', include('apps.dashboard.urls')),
 ]

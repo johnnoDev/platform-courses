ddb760024b51eba723ba979c6efd03393b6427dc
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-10 13:00:00 -0500

    Agregando el User al admin

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models.instructor import InstructorProfile
+from .models.user import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("Rol personalizado", {"fields": ("is_instructor",)}),
+    )
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        (None, {'fields': ('is_instructor',)}),
+    )
+
+
+admin.site.register(InstructorProfile)

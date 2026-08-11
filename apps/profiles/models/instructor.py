1e72cc63599e3167c5e638b6537063d52c0c3dca
johnnoDev <johnny.lopezvera20@gmail.com>
2026-08-11 10:28:22 -0500

    correcion del metodo usado en __str__

--- apps/profiles/models/instructor.py
+++ apps/profiles/models/instructor.py
@@ -16,4 +16,4 @@ class InstructorProfile(models.Model):
     social_network = models.URLField(blank=True, null=True)
     
     def __str__(self):
-        return f'Instructor: {self.get_full_name() or self.username}'
+        return f'Instructor: {self.user.get_full_name() or self.user.username}'
